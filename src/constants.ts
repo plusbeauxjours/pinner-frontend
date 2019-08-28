e5cc902a21bbce53ce4cc639cf5493b4d85fc76b
plusbeauxjours <plusbeauxjours@gmail.com>
2019-08-28 10:59:12 +0900

    commit to netlify

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 require("dotenv").config();
 
-const API_SERVER = "https://pinner-backend.herokuapp.com";
+// const MEDIA_URL = "https://pinner-backend.herokuapp.com";
+const BUCKET_URL = process.env.REACT_APP_AWS_STORAGE_BUCKET_NAME;
 export const BACKEND_URL =
-  process.env.NODE_ENV === "development" ? "http://localhost:8000" : API_SERVER;
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8000"
+    : `http://${BUCKET_URL}.s3.amazonaws.com/`;
