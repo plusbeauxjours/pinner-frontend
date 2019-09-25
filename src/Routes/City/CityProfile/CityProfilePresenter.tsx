1a098b7070ed3f4333bb74e72bf0f2164618ac7b
plusbeauxjours <plusbeauxjours@gmail.com>
2019-09-25 11:30:43 +0900

    small bug fix

--- src/Routes/City/CityProfile/CityProfilePresenter.tsx
+++ src/Routes/City/CityProfile/CityProfilePresenter.tsx
@@ -670,7 +670,7 @@ const CityProfilePresenter: React.FunctionComponent<IProps> = ({
                 <LikeBtnAlign>
                   <CityLikeBtn
                     isLiked={city.isLiked}
-                    cityId={city.id}
+                    cityId={city.cityId}
                     likeCount={city.likeCount}
                     type={"row"}
                   />
