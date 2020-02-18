485316e650669c36e707b165c1e6c4c7f7670132
plusbeauxjours <plusbeauxjours@gmail.com>
2020-02-18 19:18:50 +0900

    pinner.fun

--- src/Components/Router.tsx
+++ src/Components/Router.tsx
@@ -6,13 +6,13 @@ import {
   Redirect
 } from "react-router-dom";
 
-import HomeHome from "../Routes/Login/HomeHome";
-// import Home from "../Routes/Login/Home";
-// import Verification from "../Routes/Login/Verification";
-// import EditEmailAddress from "../Routes/User/EditEmailAddress";
-// import Approach from "../Routes/Login/Approach";
-// import NotFound from "./NotFound";
-// import NoValid from "./NoValid";
+// import HomeHome from "../Routes/Login/HomeHome";
+import Home from "../Routes/Login/Home";
+import Verification from "../Routes/Login/Verification";
+import EditEmailAddress from "../Routes/User/EditEmailAddress";
+import Approach from "../Routes/Login/Approach";
+import NotFound from "./NotFound";
+import NoValid from "./NoValid";
 
 import LoggedInPages from "./LoggedInPages";
 
@@ -24,14 +24,14 @@ interface IProps {
 
 const LoggedOutPages = () => (
   <Switch>
-    <Route path="/" exact={true} component={HomeHome} />
-    {/* <Route path="/" exact={true} component={Home} />
+    {/* <Route path="/" exact={true} component={HomeHome} /> */}
+    <Route path="/" exact={true} component={Home} />
     <Route path="/verification/:key" component={Verification} />
     <Route path="/confirm/:key" component={EditEmailAddress} />
     <Route path="/verification" component={Verification} />
     <Route path="/approach" component={Approach} />
     <Route path="/404" exact={true} component={NotFound} />
-    <Route path="/novalid" exact={true} component={NoValid} /> */}
+    <Route path="/novalid" exact={true} component={NoValid} />
     <Redirect from="*" to="/" />
   </Switch>
 );
